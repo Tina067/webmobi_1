db540a555ab5c17e7860287d6870d32d6db7545d
tina067 <tinamurmu222@gmail.com>
2024-06-15 17:30:04 +0530

    login and signup page done

---
+++ frontend/src/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
+    <h2>Sign-In</h2>
+        <div className='bg-white p-3 rounded w-25'>
+            <form action="">
+                <div className="mb-3">
+                    <label htmlFor="email"><strong>Email</strong></label>
+                    <input type="email" placeholder='Enter Email' className='form-control rounded-0'/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password"><strong>Password</strong></label>
+                    <input type="password" placeholder='Enter Password' className='form-control rounded-0'/>
+                </div>
+                <button className='btn btn-success w-100 rounded-0' ><strong>Log in</strong></button>
+                <p>Don't have an Account ?</p>
+                <Link to = '/signup' className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Create Account</Link>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
